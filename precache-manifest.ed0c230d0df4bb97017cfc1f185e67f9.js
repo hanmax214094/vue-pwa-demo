69dbe308f1f898bd6dc7b290764892360a9e671d
shchueh <sh.chueh.advtek@auo.com>
2021-02-05 16:05:03 +0800

    deploy

---
+++ precache-manifest.ed0c230d0df4bb97017cfc1f185e67f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea572576910556bf09af",
+    "url": "/vue-pwa-demo/css/chunk-vendors.5383b1fe.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vue-pwa-demo/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa-demo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "278ceb175692f74ab9b9c2b31f8b6ea6",
+    "url": "/vue-pwa-demo/index.html"
+  },
+  {
+    "revision": "c2104cdd3a823edc2a90",
+    "url": "/vue-pwa-demo/js/about.44a7c35d.js"
+  },
+  {
+    "revision": "1513f6fef4ee080f631e",
+    "url": "/vue-pwa-demo/js/app.ecd0ee43.js"
+  },
+  {
+    "revision": "ea572576910556bf09af",
+    "url": "/vue-pwa-demo/js/chunk-vendors.d6ee05aa.js"
+  },
+  {
+    "revision": "706df87e5a9d2b087a03f8fda3f91eed",
+    "url": "/vue-pwa-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa-demo/robots.txt"
+  }
+]);
